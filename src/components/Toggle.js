293ec12cbc44cd054a58bc1040ec44f1dfc67957
chadimessmer <chadi.messmer@gmail.com>
2022-02-27 17:06:44 +0100

    lab width/height

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const Toggle = ({ children, thisTitle, thisSubTitle }) => {
-  const [toggle, setToggle] = useState(false);
-  const show = () => {
-    setToggle(!toggle);
-    console.log(toggle);
-  };
-  return (
-    <motion.div layout>
-      <motion.h2 layout onClick={show} className="toggle">
-        {thisTitle}
-      </motion.h2>
-      <motion.h3 layout>{thisSubTitle}</motion.h3>
+	const [toggle, setToggle] = useState(false);
+	const show = () => {
+		setToggle(!toggle);
+		console.log(toggle);
+	};
+	return (
+		<motion.div layout>
+			<motion.h2 layout onClick={show} className="toggle">
+				{thisTitle}
+			</motion.h2>
+			<motion.h3 layout>{thisSubTitle}</motion.h3>
 
-      {toggle ? children : ""}
-    </motion.div>
-  );
+			{toggle ? children : ""}
+		</motion.div>
+	);
 };
 
 export default Toggle;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,72 +10,72 @@ import Banner from "../components/Banner";
 
 const lay = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let i = lay.length,
-  k,
-  temp;
+	k,
+	temp;
 while (--i > 0) {
-  k = Math.floor(Math.random() * (i + 1));
-  temp = lay[k];
-  lay[k] = lay[i];
-  lay[i] = temp;
+	k = Math.floor(Math.random() * (i + 1));
+	temp = lay[k];
+	lay[k] = lay[i];
+	lay[i] = temp;
 }
 
 const Home = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadTraces());
-  }, [dispatch]);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(loadTraces());
+	}, [dispatch]);
 
-  const { trace, articles, chatrooms, vision, visionary } = useSelector((state) => state.traces);
-  document.body.style.overflow = "auto";
-  trace.sort((a, b) => {
-    return a.id - b.id; //this will sort according to .id descending
-  });
+	const { trace, articles, chatrooms, vision, visionary } = useSelector((state) => state.traces);
+	document.body.style.overflow = "auto";
+	trace.sort((a, b) => {
+		return a.id - b.id; //this will sort according to .id descending
+	});
 
-  const layEnter = "lay" + lay[lay.length - 1] + " enter-container trace";
-  const layLounge = "lay" + lay[lay.length - 2] + " lounge-container trace";
+	const layEnter = "lay" + lay[lay.length - 1] + " enter-container trace";
+	const layLounge = "lay" + lay[lay.length - 2] + " lounge-container trace";
 
-  let lien = "/";
-  return (
-    <div className="home">
-      <div className="banner-container-lab">
-        <div>
-          <Link to={"/"}>LABYRINTH</Link>
-        </div>
-      </div>
-      <div className="home-links-container">
-        <Link to="labyrinth">
-          <div className={layEnter}>
-            <div key={uuid()} className="title-box-enter">
-              <div className="enter-box-background">
-                <div className="back-one"></div>
-                <div className="back-two"></div>
-                <div className="back-three"></div>
-                <div className="back-four"></div>
-              </div>
-              <h3 className="title-intro">Enter</h3>
-            </div>
-          </div>
-        </Link>
+	let lien = "/";
+	return (
+		<div className="home">
+			<div className="banner-container-lab">
+				<div>
+					<Link to={"/"}>LABYRINTH</Link>
+				</div>
+			</div>
+			<div className="home-links-container">
+				<Link to="labyrinth">
+					<div className={layEnter}>
+						<div key={uuid()} className="title-box-enter">
+							<div className="enter-box-background">
+								<div className="back-one"></div>
+								<div className="back-two"></div>
+								<div className="back-three"></div>
+								<div className="back-four"></div>
+							</div>
+							<h3 className="title-intro">Enter</h3>
+						</div>
+					</div>
+				</Link>
 
-        {trace.map((trace, index) => (
-          <Link key={uuid()} to={"/trace/" + trace.id}>
-            <Trace lay={lay[index]} index={index} title={trace.attributes.title} />
-          </Link>
-        ))}
-        <Link to="/lounge">
-          <div className={layLounge}>
-            <div className="title-box">
-              <div className="title-box-background lounge-intro"></div>
-              <h3 className="title-intro lounge-text">Lounge</h3>
-            </div>
-          </div>
-        </Link>
-      </div>
-      <div className="newsletter">
-        <a href="#">NEWSLETTER</a>
-      </div>
-    </div>
-  );
+				{trace.map((trace, index) => (
+					<Link key={uuid()} to={"/trace/" + trace.id}>
+						<Trace lay={lay[index]} index={index} title={trace.attributes.title} />
+					</Link>
+				))}
+				<Link to="/lounge">
+					<div className={layLounge}>
+						<div className="title-box">
+							<div className="title-box-background lounge-intro"></div>
+							<h3 className="title-intro lounge-text">Lounge</h3>
+						</div>
+					</div>
+				</Link>
+			</div>
+			<div className="newsletter">
+				<a href="#">NEWSLETTER</a>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
